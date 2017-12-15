b27a501dfd334b65982cd5eb3647d579544839f9
Tripp Lyons <tripplyons@gmail.com>
2017-12-15 01:22:26 +0000

    Create ha.sh

---
+++ ha.sh
@@ -0,0 +1,6 @@
+echo
+URL=https://tripplyons.github.io/base64bin/#$(echo $1 | python -m base64)
+echo $URL
+wget -qO- http://tinyurl.com/api-create.php\?url=$(python -c "import urllib, sys; print urllib.quote(sys.argv[1])" $URL)
+echo
+echo
