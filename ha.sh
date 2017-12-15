cfa712aed7e63edc5dc1cb22a372927d4798b79c
Tripp Lyons <tripplyons@gmail.com>
2017-12-15 09:18:04 -0800

    hash file instead of text

--- ha.sh
+++ ha.sh
@@ -1,5 +1,5 @@
 echo
-URL=https://tripplyons.github.io/base64bin/#$(echo $1 | python -m base64)
+URL=https://tripplyons.github.io/base64bin/#$(cat $1 | python -m base64)
 echo $URL
 wget -qO- http://tinyurl.com/api-create.php\?url=$(python -c "import urllib, sys; print urllib.quote(sys.argv[1])" $URL)
 echo
